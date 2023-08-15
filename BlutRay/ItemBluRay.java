090c06f688b0587f801faf3d1e42fc5e916a1e1a
Cristian Maciel Pereira <107712125+cristianmaciel@users.noreply.github.com>
2023-08-15 18:10:16 -0300

    ItemBluRay.java

---
+++ BlutRay/ItemBluRay.java
@@ -0,0 +1,26 @@
+package BluRayInterface;
+
+public class ItemBluRay {
+    //atributos
+    private double preco;
+    private String nomeCliente;
+    //metodos gets e sets
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+    
+    
+}
